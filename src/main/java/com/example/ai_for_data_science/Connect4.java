e9ce431ed470f18aebb9fb7eccb45cf02af432ed
Maledetti <116837517+Maledetti@users.noreply.github.com>
2022-04-11 19:58:12 +0000

    permissions

--- src/main/java/com/example/ai_for_data_science/Connect4.java
+++ src/main/java/com/example/ai_for_data_science/Connect4.java
@@ -1,4 +1,6 @@
 package com.example.ai_for_data_science;
 
 
-import com.example.ai_for_data_science.players.algorit
+import com.example.ai_for_data_science.players.algorithms.BayesianClassifier;
+import com.example.ai_for_data_science.players.algorithms.Human;
+im
