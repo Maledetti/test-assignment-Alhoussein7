b86cafef3000fb63bb29b8bf34d9804e97731db0
Maledetti <116837517+Maledetti@users.noreply.github.com>
2022-04-13 11:03:09 +0000

    [maven-release-plugin] [skip ci] prepare for next development iteration

--- src/main/java/com/example/ai_for_data_science/Connect4.java
+++ src/main/java/com/example/ai_for_data_science/Connect4.java
@@ -4,4 +4,8 @@ package com.example.ai_for_data_science;
 import com.example.ai_for_data_science.players.algorithms.BayesianClassifier;
 import com.example.ai_for_data_science.players.algorithms.Human;
 import com.example.ai_for_data_science.players.algorithms.svm.AlgorithmsWinningScores;
-import com.example.ai_for_data_science.players.algorithms.svm.ScorePl
+import com.example.ai_for_data_science.players.algorithms.svm.ScorePlayers;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import jav
