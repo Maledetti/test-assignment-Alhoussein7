a4690626f7ffd2ee0833d3b13f1e23239f7f5aa8
Maledetti <116837517+Maledetti@users.noreply.github.com>
2022-04-13 08:16:33 +0000

    0.1.31

--- src/main/java/com/example/ai_for_data_science/Connect4.java
+++ src/main/java/com/example/ai_for_data_science/Connect4.java
@@ -4,3 +4,4 @@ package com.example.ai_for_data_science;
 import com.example.ai_for_data_science.players.algorithms.BayesianClassifier;
 import com.example.ai_for_data_science.players.algorithms.Human;
 import com.example.ai_for_data_science.players.algorithms.svm.AlgorithmsWinningScores;
+import com.example.ai_for_data_science.players.algorithms.svm.ScorePl
