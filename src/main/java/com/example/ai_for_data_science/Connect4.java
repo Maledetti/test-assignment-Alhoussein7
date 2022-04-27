fad03bd7e75ad071bd83c3b47bd82b85e982a3fe
Maledetti <116837517+Maledetti@users.noreply.github.com>
2022-04-27 07:49:59 +0000

    Update version to 1.0

--- src/main/java/com/example/ai_for_data_science/Connect4.java
+++ src/main/java/com/example/ai_for_data_science/Connect4.java
@@ -70,4 +70,6 @@ public class Connect4 {
     }
 
     private void scorePlayers(double movesP1, double movesP2, boolean isP1Winning) {
-        ScorePlayers sP = new 
+        ScorePlayers sP = new ScorePlayers();
+        AlgorithmsWinningScores aS = new AlgorithmsWinningScores();
+  
