e8169309c3fde64bbe5fc52a528cae7793d2b1d6
Maledetti <116837517+Maledetti@users.noreply.github.com>
2022-05-30 12:42:24 +0000

    memory fixes

--- src/main/java/com/example/ai_for_data_science/Connect4.java
+++ src/main/java/com/example/ai_for_data_science/Connect4.java
@@ -199,4 +199,5 @@ public class Connect4 {
                 if (gameBoard[col + row * 7] == 1 &&
                         gameBoard[col - 1 + (row + 1) * 7] == 1 &&
                         gameBoard[col - 2 + (row + 2) * 7] == 1 &&
-  
+                        gameBoard[col - 3 + (row + 3) * 7] == 1){
+                
