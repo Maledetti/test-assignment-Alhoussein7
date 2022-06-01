29694fc8ebb15ac74ccfc5d56a693b855b09d987
Maledetti <116837517+Maledetti@users.noreply.github.com>
2022-06-01 10:22:42 +0000

    uninitialized variable

--- src/main/java/com/example/ai_for_data_science/Connect4.java
+++ src/main/java/com/example/ai_for_data_science/Connect4.java
@@ -206,4 +206,8 @@ public class Connect4 {
                         gameBoard[col - 1 + (row + 1) * 7] == 2 &&
                         gameBoard[col - 2 + (row + 2) * 7] == 2 &&
                         gameBoard[col - 3 + (row + 3) * 7] == 2){
-                    return 2; // player 2 has
+                    return 2; // player 2 has 4 diagonal discs in a row
+                }
+            }
+        }
+ 
