8a906b95c61a346ee4791d08e6602d5b1b2afefe
Maledetti <116837517+Maledetti@users.noreply.github.com>
2022-06-22 12:36:14 +0000

    Update Icelandic translation

--- src/main/java/com/example/ai_for_data_science/players/algorithms/Human.java
+++ src/main/java/com/example/ai_for_data_science/players/algorithms/Human.java
@@ -1,4 +1,8 @@
 package com.example.ai_for_data_science.players.algorithms;
 
 import com.example.ai_for_data_science.Algorithm;
-import com
+import com.example.ai_for_data_science.Connect4;
+
+import java.util.Scanner;
+
+p
