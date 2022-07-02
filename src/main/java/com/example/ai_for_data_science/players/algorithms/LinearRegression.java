bd7d71deebfab98e5c70de7da06d053dfe923f24
Maledetti <116837517+Maledetti@users.noreply.github.com>
2022-07-02 09:11:51 +0000

    msm: vidc: add kzalloc success check for new q6 HAL session

--- src/main/java/com/example/ai_for_data_science/players/algorithms/LinearRegression.java
+++ src/main/java/com/example/ai_for_data_science/players/algorithms/LinearRegression.java
@@ -122,3 +122,74 @@ public class LinearRegression implements Algorithm {
         System.out.println("Linear Regression - Results:");
         System.out.println("  Weights: " + Arrays.toString(weights));
         System.out.println("  Bias: " + bias);
+        System.out.println("  MSE: " + mseCost());
+        System.out.println("  R²: " + Math.round(rSquared() * 100000.0f) / 1000.0f + "%");
+        System.out.println("\n  Accuracy: " + calculateAccuracy());
+    }
+
+
+    public static void preProcessData() throws IOException {
+//        BufferedWriter writer = new BufferedWriter(new FileWriter("winRates.csv", true));
+//
+//        for (int i = 0; i < 42; i++) {      // for each cell
+//            double totalPlays = 0.0d;
+//            int totalP1Plays = 0;
+//            int winsP1 = 0;
+//
+//            Scanner scanner = new Scanner(new File("gameData.csv"));
+//            while (scanner.hasNextLine()) {
+//                String line = scanner.nextLine();
+//                String gameResult = line.split(",")[1];
+//
+//                char cellValue = line.charAt(i);
+//                if (cellValue == '1') {
+//                    if (gameResult.equals("1")) {
+//                        ++winsP1;
+//                    }
+//                    ++totalP1Plays;
+//                }
+//                if (cellValue == '1' || cellValue == '2') {
+//                    double nominator = ((i / 7) * 7);
+//                    nominator = nominator == 0 ? 1 : nominator;
+//                    totalPlays += (double)1 / nominator;
+//                }
+//            }
+//
+//            String winRate = totalP1Plays == 0 ? "" : String.valueOf((double)winsP1/totalP1Plays);
+//
+//            String totalPlays_s = String.valueOf(totalPlays);
+//            writer.write(String.format("%d,%d,%d,%s,%s\n", i, i % 7, totalP1Plays, totalPlays_s, winRate));
+//        }
+//        writer.close();
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter("winRates.csv", true));
+
+        for (int i = 0; i < 42; i++) {      // for each cell
+            int totalP1Plays = 0;
+            int winsP1 = 0;
+
+            Scanner scanner = new Scanner(new File("gameData.csv"));
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String gameResult = line.split(",")[1];
+
+                char cellValue = line.charAt(i);
+                if (cellValue == '1') {
+                    if (gameResult.equals("1")) {
+                        ++winsP1;
+                    }
+                    ++totalP1Plays;
+                }
+            }
+
+            String winRate = totalP1Plays == 0 ? "" : String.valueOf((double)winsP1/totalP1Plays);
+
+            String artificialGameBoard_s = "";
+            for (int j = 0; j < 42; j++) {
+                if (j == i) {
+                    artificialGameBoard_s += "1,";
+                }
+                else {
+                    artificialGameBoard_s += "0,";
+                }
+            }
