d17d4706168c93c51e0244cb728d23214fd30404
Maledetti <116837517+Maledetti@users.noreply.github.com>
2022-07-03 22:36:55 +0000

    Trying to set up Docker with CircleCI

--- src/main/java/com/example/ai_for_data_science/players/algorithms/Minimax.java
+++ src/main/java/com/example/ai_for_data_science/players/algorithms/Minimax.java
@@ -2,4 +2,8 @@ package com.example.ai_for_data_science.players.algorithms;
 
 import com.example.ai_for_data_science.Algorithm;
 import com.example.ai_for_data_science.Connect4;
-import com.example.ai_f
+import com.example.ai_for_data_science.DataSet;
+
+public class Minimax implements Algorithm {
+
+    dou
