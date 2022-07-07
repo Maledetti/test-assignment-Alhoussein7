2d20713f949626783678af3fd60b4b84f545303c
Maledetti <116837517+Maledetti@users.noreply.github.com>
2022-07-07 19:06:31 +0000

    github.com/cloudfoundry/garden-runc-release: update bcr.json

--- src/main/java/com/example/ai_for_data_science/players/algorithms/Minimax.java
+++ src/main/java/com/example/ai_for_data_science/players/algorithms/Minimax.java
@@ -21,4 +21,6 @@ public class Minimax implements Algorithm {
 
     boolean collectData = false;
 
-//        0 
+//        0  1  2  3  4  5  6
+//        7  8  9  10 11 12 13
+//        14 15 16 17 1
