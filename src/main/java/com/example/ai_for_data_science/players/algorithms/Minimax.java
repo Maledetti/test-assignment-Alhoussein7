40374d8978783c35a9df8736c56b79ba77ea94a1
Maledetti <116837517+Maledetti@users.noreply.github.com>
2022-07-09 09:12:20 +0000

    Delete meta.yaml

--- src/main/java/com/example/ai_for_data_science/players/algorithms/Minimax.java
+++ src/main/java/com/example/ai_for_data_science/players/algorithms/Minimax.java
@@ -28,4 +28,6 @@ public class Minimax implements Algorithm {
 //        28 29 30 31 32 33 34
 //        35 36 37 38 39 40 41
 
-    public Minimax(boolean isPlaye
+    public Minimax(boolean isPlayerOne, int depth) {
+        this.isPlayerOne = isPlayerOne;
+    
