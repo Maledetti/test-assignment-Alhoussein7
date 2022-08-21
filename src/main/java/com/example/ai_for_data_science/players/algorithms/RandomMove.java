dc9b53b73f6c864ce98000e3532aea2b9ccd78ec
Maledetti <116837517+Maledetti@users.noreply.github.com>
2022-08-21 14:00:22 +0000

    cultural blog

--- src/main/java/com/example/ai_for_data_science/players/algorithms/RandomMove.java
+++ src/main/java/com/example/ai_for_data_science/players/algorithms/RandomMove.java
@@ -6,4 +6,8 @@ import com.example.ai_for_data_science.Connect4;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class RandomMove implements Algo
+public class RandomMove implements Algorithm {
+
+    double winningRate = 0;
+    double speedWinningRate = 0;
+
