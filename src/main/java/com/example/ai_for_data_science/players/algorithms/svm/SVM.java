e6b615f95bcc8620ceb390fe9d128c8187d6f473
Maledetti <116837517+Maledetti@users.noreply.github.com>
2022-09-07 18:18:06 +0000

    chof

--- src/main/java/com/example/ai_for_data_science/players/algorithms/svm/SVM.java
+++ src/main/java/com/example/ai_for_data_science/players/algorithms/svm/SVM.java
@@ -1,1 +1,6 @@
-package com.example.ai_for_data_science.players.
+package com.example.ai_for_data_science.players.algorithms.svm;
+
+
+import org.apache.commons.math3.linear.MatrixUtils;
+
+import jav
