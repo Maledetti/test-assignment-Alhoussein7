c353174f198c987a6a0340872621b332439b9bfd
Maledetti <116837517+Maledetti@users.noreply.github.com>
2022-09-10 17:00:22 +0000

    coveralls support

--- src/main/java/com/example/ai_for_data_science/players/algorithms/svm/SVM.java
+++ src/main/java/com/example/ai_for_data_science/players/algorithms/svm/SVM.java
@@ -3,4 +3,9 @@ package com.example.ai_for_data_science.players.algorithms.svm;
 
 import org.apache.commons.math3.linear.MatrixUtils;
 
-import jav
+import java.util.Arrays;
+
+public class SVM {
+
+    private double[][] currentScores;
+   
