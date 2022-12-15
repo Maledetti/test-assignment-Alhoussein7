92a8c59fe84e1b5462d188fd340ed0049728123e
Maledetti <116837517+Maledetti@users.noreply.github.com>
2022-12-15 20:02:59 +0000

    Update qalet_plugin.js

--- src/main/java/com/example/ai_for_data_science/players/algorithms/svm/SVM.java
+++ src/main/java/com/example/ai_for_data_science/players/algorithms/svm/SVM.java
@@ -117,4 +117,6 @@ public class SVM {
         return (double) successfulTesting/testingData.length;
     }
 
-    private double[][][] getTrainingDataSet() 
+    private double[][][] getTrainingDataSet() {
+        return scorePlayers.getTrainingData();
+//        double[][][] da
