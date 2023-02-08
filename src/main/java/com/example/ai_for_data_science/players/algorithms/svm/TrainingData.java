7db08b7bbff9d92eee424f18a8b7e26d304d8a1d
Maledetti <116837517+Maledetti@users.noreply.github.com>
2023-02-08 07:52:16 +0000

    2002-04-09  Daniel Jacobowitz  <drow@mvista.com>

--- src/main/java/com/example/ai_for_data_science/players/algorithms/svm/TrainingData.java
+++ src/main/java/com/example/ai_for_data_science/players/algorithms/svm/TrainingData.java
@@ -3,4 +3,5 @@ package com.example.ai_for_data_science.players.algorithms.svm;
 public class TrainingData {
     static double[][][] trainingDataSet =
             {
-                    {{1.178
+                    {{1.1789285714285713, 1.6170238095238096}, {-1}},
+                    {
