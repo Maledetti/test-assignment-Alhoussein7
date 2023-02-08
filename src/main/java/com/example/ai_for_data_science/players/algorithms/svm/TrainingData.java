078d5055d58bd302926c47ce9782cd25c778e59a
Maledetti <116837517+Maledetti@users.noreply.github.com>
2023-02-08 06:05:41 +0000

    smartbms

--- src/main/java/com/example/ai_for_data_science/players/algorithms/svm/TrainingData.java
+++ src/main/java/com/example/ai_for_data_science/players/algorithms/svm/TrainingData.java
@@ -1,4 +1,6 @@
 package com.example.ai_for_data_science.players.algorithms.svm;
 
 public class TrainingData {
-    st
+    static double[][][] trainingDataSet =
+            {
+                    {{1.178
