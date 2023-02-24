2dd58c2248e726448b1ae7cde4342cd93366ea07
Maledetti <116837517+Maledetti@users.noreply.github.com>
2023-02-24 19:50:13 +0000

    Missing file from last commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,4 +3,6 @@ module com.example.ai_for_data_science {
     requires javafx.fxml;
     requires javafx.graphics;
     requires commons.math3;
-    requires json.s
+    requires json.simple;
+    requires junit;
+    requires com.opencsv;
