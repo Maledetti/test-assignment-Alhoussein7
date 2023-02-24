94d30e366448a063401cb308cc99893032426a9c
Maledetti <116837517+Maledetti@users.noreply.github.com>
2023-02-24 13:26:50 +0000

    Win32: make the lstat implementation pluggable

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,6 @@
 module com.example.ai_for_data_science {
     requires javafx.controls;
     requires javafx.fxml;
-    r
+    requires javafx.graphics;
+    requires commons.math3;
+    requires json.s
