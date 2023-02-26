dd71f64827980a48c194294d9210d8447363caa7
Maledetti <116837517+Maledetti@users.noreply.github.com>
2023-02-26 09:41:17 +0000

    more comment

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,3 +6,7 @@ module com.example.ai_for_data_science {
     requires json.simple;
     requires junit;
     requires com.opencsv;
+
+
+    opens com.example.ai_for_data_science to javafx.fxml;
+    exports com
